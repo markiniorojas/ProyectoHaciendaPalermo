eea0ab6c016e5312e0edd94d1548d45789fedf7d
markiniorojas <markiniorojas@gmail.com>
2025-04-24 13:10:40 -0500

    arreglos de enlaces de paginas

--- ModelSecurityCt/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ ModelSecurityCt/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+726c7bb0d30ec57a627f7ccc33e8a6e7c9b122b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51481488ed4fc79eff042d1dbf1d22ad4867fb3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
