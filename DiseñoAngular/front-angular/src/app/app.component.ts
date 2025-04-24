ccc2682270cb5e1f530f6a9c4cd91043f76ab023
markiniorojas <markiniorojas@gmail.com>
2025-04-24 17:54:35 -0500

    angular

---
+++ DiseñoAngular/front-angular/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome Marcos!</h1>
+
+    <router-outlet />
+  `,
+  styles: [],
+})
+export class AppComponent {
+}
